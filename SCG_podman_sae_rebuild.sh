0bd6432eb0d9a7053ccbe808978f84fd8d24b0c7
TerraRoot <TerraRoot@gmail.com>
2023-11-30 11:49:29 +0000

    Update SCG_podman_sae_rebuild.sh

--- SCG_podman_sae_rebuild.sh
+++ SCG_podman_sae_rebuild.sh
@@ -5,20 +5,22 @@ SAE_CONFIG_VOLUME="/opt/dell/secureconnectgateway/config"
 SCG_BRIDGE="sae-srs-bridge"
 SAE_CONTAINER_NAME=saede-app
 HostName=$(cat /etc/hostname)
-#not so happy about that delimiter
-SAE_REPO_TAG=$(podman images | grep sae_de | cut -f 10 -d ' ')
 SAE_REPO_NAME=sae_de
 security_opt="--security-opt label=disable"
 security_opt_sae="--security-opt seccomp=${profile_file}"
-profile_file="${volume_path}/podmanprofile.json"
+#file is one dir down from here? add /.. such hack
+profile_file="${volume_path}/../dockerprofile.json"
 pid_limit="--pids-limit=-1"
 sae_memory_cont="6.98492g"
 Deployment_Type="podman"
 
+#not so happy about that delimiter
+SAE_REPO_TAG=$(podman images | grep sae_de | cut -f 10 -d ' ')
 HOST_IP=$(podman exec -i esrsde-app cat /opt/esrsve/version/esrshost.conf | grep 'IpAddress' | cut -d'=' -f2)
 ENVIRONMENT=$(podman exec -i esrsde-app cat /opt/esrsve/version/esrshost.conf | grep 'Environment' | cut -d'=' -f2)
 MacAddress=$(podman exec -i esrsde-app cat /opt/esrsve/version/esrshost.conf | grep 'MacAddress' | cut -d'=' -f2)
 Version=$(podman exec -i esrsde-app cat /opt/esrsve/version/esrshost.conf | grep 'Version' | grep -v 'OSPatch' | cut -d'=' -f2)
+volume_path=$(podman volume inspect saede_config -f '{{ .Mountpoint }}')
 
   if [[ ! -z "$ip" && "$ip" != "${1#*[0-9].[0-9]}" ]]; then
       log "IPV4 enabled"
@@ -43,12 +45,12 @@ else
     TimeZone=$(ls -ltr /etc/localtime|awk '{print $NF}'|cut -d "/" -f5,6)
 fi
 
-# blow awway old container
+# blow away old container
 
 podman stop saede_app
 podman rm saede_app
 
-# don't know of a way right now to restore database for sae	
+# don't know of a way right now to restore database for sae, probably uses the data volume	
 # this bit is extracted from the podman install bin
 
 podman run $security_opt $security_opt_sae --memory=$sae_memory_cont --memory-swap="-1" --restart=always -v saede_data:$SAE_DATA_VOLUME \
@@ -62,16 +64,16 @@ podman run $security_opt $security_opt_sae --memory=$sae_memory_cont --memory-sw
 
 #todo
 #umm make this a script instead of a pile of notes and guesses.
-#probably test on a lab.
+#probably test on a lab. and not a customers setup
 
-
-[Yesterday 21:28] Wang, Daniel
-podman run --security-opt label=disable --restart=always --ulimit core=-1:-1 --ulimit memlock=16384000:16384000 -v saede_data:/opt/dell/secureconnectgateway/data \
- -v saede_logs:/opt/dell/secureconnectgateway/logs -v saede_config:/opt/dell/secureconnectgateway/config --add-host linux.site:<HOST_IP> --add-host podmanhost:<HOST_IP> \
- -v /var/run/podman/podman.sock:/var/run/podman.sock:ro -v srssae:/shared -v /etc/hostname:/etc/hostname -v /etc/hosts:/etc/hosts:ro -v /usr/bin/podman:/podmanCli/podman -e ESRS_ADMIN=admin\
- -e IP_ADDRESS=<HOST_IP> -e TIME_ZONE=<TIMEZONE> -e 'HYPERVISOR_TYPE=Container Platform - podman' -e 'OS=SUSE Linux Enterprise Server 12 SP5' -e ENVIRONMENT=PROD -e HOSTNAME=<HOSTNAME> -e MAC_ADDRESS=<HOST_MAC>\
- -e DEPLOYMENT_TYPE=podman --device=/dev/mem:/dev/mem --network sae-srs-bridge -d -p 5700:5700 -p 5701:5701 -p 5702:5702 -p 5703:5703 -p 5704:5704 -p 162:162/udp -p 162:162/tcp -p 161:161/udp -p 161:161/tcp \
- --cap-add=SYS_PTRACE --cap-add=SYS_RAWIO --cap-add=CAP_AUDIT_WRITE --cap-add=CAP_NET_BIND_SERVICE --name saede-app sae_de:<TAG>
+#earlier code extract
+#[Yesterday 21:28] Wang, Daniel
+#podman run --security-opt label=disable --restart=always --ulimit core=-1:-1 --ulimit memlock=16384000:16384000 -v saede_data:/opt/dell/secureconnectgateway/data \
+# -v saede_logs:/opt/dell/secureconnectgateway/logs -v saede_config:/opt/dell/secureconnectgateway/config --add-host linux.site:<HOST_IP> --add-host podmanhost:<HOST_IP> \
+# -v /var/run/podman/podman.sock:/var/run/podman.sock:ro -v srssae:/shared -v /etc/hostname:/etc/hostname -v /etc/hosts:/etc/hosts:ro -v /usr/bin/podman:/podmanCli/podman -e ESRS_ADMIN=admin\
+# -e IP_ADDRESS=<HOST_IP> -e TIME_ZONE=<TIMEZONE> -e 'HYPERVISOR_TYPE=Container Platform - podman' -e 'OS=SUSE Linux Enterprise Server 12 SP5' -e ENVIRONMENT=PROD -e HOSTNAME=<HOSTNAME> -e MAC_ADDRESS=<HOST_MAC>\
+# -e DEPLOYMENT_TYPE=podman --device=/dev/mem:/dev/mem --network sae-srs-bridge -d -p 5700:5700 -p 5701:5701 -p 5702:5702 -p 5703:5703 -p 5704:5704 -p 162:162/udp -p 162:162/tcp -p 161:161/udp -p 161:161/tcp \
+# --cap-add=SYS_PTRACE --cap-add=SYS_RAWIO --cap-add=CAP_AUDIT_WRITE --cap-add=CAP_NET_BIND_SERVICE --name saede-app sae_de:<TAG>
  
  
  
@@ -100,6 +102,6 @@ podman run --security-opt label=disable --restart=always --ulimit core=-1:-1 --u
 #$3 looks like we don't have a auot way to do so in orignal?
 
 
-#volume_path=$(podman volume inspect saede_config -f '{{ .Mountpoint }}')
+#
 #volume_path=${volume_path%/*}
 #hostMount=$(echo $volume_path | awk -F'/saede_config' '{print $1}')
