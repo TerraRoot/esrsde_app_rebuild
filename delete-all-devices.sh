7b898910e186aece656563f94b4c8edea42b5054
TerraRoot <TerraRoot@gmail.com>
2024-01-26 19:58:15 +0000

    Update delete-all-devices.sh
    Passwords removed

--- delete-all-devices.sh
+++ delete-all-devices.sh
@@ -2,7 +2,7 @@
 # this file deletes all devices from a PM 5.x, it needs to run inside the container
 printf "Starting reading devices from database\n"
 printf "\n"
-devices=$(curl --silent -X POST "http://localhost:5984/device/_find" -H "Authorization: Basic YWRtaW46I3RaOVcxaUgwVng4dTk=" -H "accept: application/json" -H "Content-Type: application/json" -d  "{\"selector\": { \"_id\" : {\"\$regex\" : \"device-\"}}, \"fields\": [  \"_id\",\"_rev\" ]}" | grep -E "^{\"_id")
+devices=$(curl --silent -X POST "http://localhost:5984/device/_find" -H "Authorization: Basic =" -H "accept: application/json" -H "Content-Type: application/json" -d  "{\"selector\": { \"_id\" : {\"\$regex\" : \"device-\"}}, \"fields\": [  \"_id\",\"_rev\" ]}" | grep -E "^{\"_id")
 
 printf "Registries found:\n"
 printf "${devices[@]}"
@@ -16,7 +16,7 @@ for d in ${devices[@]}; do
    rev=$(echo $d | cut -d',' -f2 | cut -d':' -f2 | sed 's/"//g' | sed 's/.$//' | sed 's/}//')
    printf "rev: $rev\n"
    printf "DELETE /device/$id?rev=$rev\n"
-   curl -X DELETE "http://localhost:5984/device/$id?rev=$rev" -H "Authorization: Basic YWRtaW46I3RaOVcxaUgwVng4dTk="
+   curl -X DELETE "http://localhost:5984/device/$id?rev=$rev" -H "Authorization: Basic ="
 done
 
 printf "________END________\n":
